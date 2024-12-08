89511c97d225b8fe713d2b9b7d29060c40b953d3
mbailey <meganbailey@sandiego.edu>
2024-12-08 11:32:14 -0800

    day 4 s2

--- solutions/d4.js
+++ solutions/d4.js
@@ -49,6 +49,23 @@ function matchNegativeDiagonalBkwd(i, j, arr) {
     return false
 }
 
+function matchMASLeft(i, j, arr) {
+    if (( i+2 < arr.length && j+2 < arr[i].length ) && arr[i][j] === 'M' && arr[i+1][j+1] === 'A' &&  arr[i+2][j+2] === 'S')
+        return true;
+    if (( i+2 < arr.length && j+2 < arr[i].length ) && arr[i][j] === 'S' && arr[i+1][j+1] === 'A' &&  arr[i+2][j+2] === 'M')
+        return true;
+    return false;
+}
+
+function matchMASRight(i, j, arr) {
+    if (( i+2 < arr.length && j-2 >= 0 ) && arr[i][j] === 'M' && arr[i+1][j-1] === 'A' &&  arr[i+2][j-2] === 'S')
+        return true;
+    if (( i+2 < arr.length && j-2 >= 0 ) && arr[i][j] === 'S' && arr[i+1][j-1] === 'A' &&  arr[i+2][j-2] === 'M')
+        return true;
+    return false;
+}
+
+
 
 fs.readFile('./inputs/inputd4.txt', (err, rawData) => {
     if (err) throw err;
@@ -65,7 +82,9 @@ fs.readFile('./inputs/inputd4.txt', (err, rawData) => {
     let count = 0;
     for ( i = 0; i < crosswordData.length; i++ ) {
         for ( j = 0; j < crosswordData[i].length; j++ ) {
-            if ( matchHorizontalFwd(i, j, crosswordData) )
+            
+            // S1
+            /* if ( matchHorizontalFwd(i, j, crosswordData) )
                 count++;
             if ( matchHorizontalBkwd(i, j, crosswordData) )
                 count++;
@@ -80,6 +99,10 @@ fs.readFile('./inputs/inputd4.txt', (err, rawData) => {
             if ( matchNegativeDiagonalBkwd(i, j, crosswordData) )
                 count++;
             if ( matchNegativeDiagonalFwd(i, j, crosswordData) )
+                count++; */
+
+            // S2
+            if ( matchMASLeft(i, j-1, crosswordData) && matchMASRight(i, j+1, crosswordData) )
                 count++;
         }
     }

df23f4f3c55821d837dc25a4a84030d9bf379b5f
mbailey <meganbailey@sandiego.edu>
2024-12-10 19:47:04 -0800

    d5 solution2

--- solutions/d5.js
+++ solutions/d5.js
@@ -9,6 +9,17 @@ function determineRule( rule, arr ) {
     return false;
 }
 
+function reorder(rule, arr) {
+    let shouldBeBeforeIndex = arr.findIndex( a => a === rule[0])
+    let shouldBeAfterIndex = arr.findIndex( a => a === rule[1])
+    let shouldBeBefore = arr[shouldBeBeforeIndex]
+    let newArr = arr.slice(0, shouldBeAfterIndex)
+        .concat([ shouldBeBefore ])
+        .concat( arr.slice(shouldBeAfterIndex, shouldBeBeforeIndex) )
+        .concat( arr.slice(shouldBeBeforeIndex+1, arr.length) )
+    return newArr;
+}
+
 const inputFile = fs.readFileSync('./inputs/inputd5.txt',  { encoding: 'utf8', flag: 'r' })
 
 let rules = []
@@ -22,9 +33,10 @@ inputFile.split('\n').forEach(line => {
     }
 })
 
-let updatesThatFollowRules = []
+let updatesThatWereReorderd = []
 
-inputLines.forEach( inputLine => {
+// Solution 1
+/* inputLines.forEach( inputLine => {
     let evalEachNum = inputLine.map( numInLine => {
         let applicableRules = rules.filter( rule => rule.includes(numInLine) && inputLine.includes(rule[0]) && inputLine.includes(rule[1]))
         let evalEachRule = applicableRules.map( rule => determineRule(rule, inputLine) )
@@ -36,10 +48,29 @@ inputLines.forEach( inputLine => {
     if ( !evalEachNum.includes(false) ) {
         updatesThatFollowRules.push(inputLine)
     }
-})
+}) */
 
-let middleNumSum = 0;
-updatesThatFollowRules.forEach( item => {
-    middleNumSum += parseInt( item[ Math.floor(item.length/2) ] )
-})
-console.log(middleNumSum)
+
+    // Solution 2
+    inputLines.forEach( inputLine => {
+        let reorderedLine = []
+        inputLine.map( numInLine => {
+            let applicableRules = rules.filter( rule => rule.includes(numInLine) && inputLine.includes(rule[0]) && inputLine.includes(rule[1]))
+            applicableRules.forEach( rule => { 
+                if ( !determineRule(rule, reorderedLine.length > 0 ? reorderedLine : inputLine) ) {
+                    reorderedLine = reorder(rule, reorderedLine.length > 0 ? reorderedLine : inputLine)
+                }
+            })
+        })
+
+        if ( reorderedLine.length > 0 ) {
+            updatesThatWereReorderd.push(reorderedLine)
+        }
+       
+    })
+
+    let middleNumSum = 0;
+    updatesThatWereReorderd.forEach( item => {
+        middleNumSum += parseInt( item[ Math.floor(item.length/2) ] )
+    })
+    console.log(middleNumSum)
